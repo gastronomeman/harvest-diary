344a9f14f92f545f52c80ec2fe4ec1eab29642c7
朱铭俊 <1797171444@qq.com>
2023-12-26 16:18:52 +0800

    calenaerapp文件未成功运行，maven里面添加了calendarview，setting页布局完成百分之40

---
+++ src/main/java/com/HarvestDiary/otherTools/calenderapp.java
@@ -0,0 +1,68 @@
+package com.HarvestDiary.otherTools;
+
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class calenderapp extends Application  {
+    @Override
+    public void start(Stage stage) throws Exception {
+        CalendarView calendarView=new CalendarView();
+
+        Calendar birthdays=new Calendar("birthdays");
+
+        CalendarSource calendarSource=new CalendarSource("my Calendars");
+
+        calendarSource.getCalendars().addAll(birthdays);
+
+        calendarView.getCalendarSources().add(calendarSource);
+
+        calendarView.setRequestedTime(LocalTime.now());
+
+        Thread updateTimeThread =new Thread("Calendar:update Time Thread"){
+            @Override
+            public void run() {
+                while (true){
+                    Platform.runLater(()->{
+                        calendarView.setToday(LocalDate.now());
+                        calendarView.setTime(LocalTime.now());
+                    });
+                    try {
+//                        每十秒1刷新
+                        sleep(1000);
+
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            };
+        };
+        updateTimeThread.setPriority(Thread.MIN_PRIORITY);
+
+        updateTimeThread.setDaemon(true);
+        updateTimeThread.start();
+
+        Scene scene=new Scene(calendarView);
+
+        stage.setTitle("更吊的日历类");
+        stage.setScene(scene);
+        stage.setWidth(1300);
+        stage.setHeight(1000);
+
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
