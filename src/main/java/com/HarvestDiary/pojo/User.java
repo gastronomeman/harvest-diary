490b3f23ef5925f8cc24d310a19caa0f8c019c96
gastronome <1305573134@qq.com>
2023-12-26 21:45:55 +0800

    添加类

---
+++ src/main/java/com/HarvestDiary/pojo/User.java
@@ -0,0 +1,16 @@
+package com.HarvestDiary.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String userNumber;
+    String username;
+    String password;
+    String phone;
+
+}
