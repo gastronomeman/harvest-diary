1594a78a089cdc82fc291c12783a8d91baa10a9d
gastronome <1305573134@qq.com>
2023-12-27 19:10:15 +0800

    添加“忘记密码”页面的ui

---
+++ src/main/java/com/HarvestDiary/Ui/ForgotPassword.java
@@ -0,0 +1,14 @@
+package com.HarvestDiary.Ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ForgotPassword extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
