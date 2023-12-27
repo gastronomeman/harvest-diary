6c386a04372036de4f1cc36101219d7f0aa563b3
gastronome <1305573134@qq.com>
2023-12-27 16:26:55 +0800

    更新工具类，添加许多对文件的管理

--- src/main/java/com/HarvestDiary/otherTools/OperationalDocument.java
+++ src/main/java/com/HarvestDiary/otherTools/OperationalDocument.java
@@ -4,14 +4,21 @@ import cn.hutool.core.codec.Base64;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.io.file.FileWriter;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
 import cn.hutool.crypto.symmetric.SymmetricCrypto;
 import cn.hutool.json.JSONUtil;
+import com.HarvestDiary.pojo.Poetry;
 import com.HarvestDiary.pojo.User;
 import lombok.extern.slf4j.Slf4j;
 
+
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class OperationalDocument {
@@ -19,19 +26,23 @@ public class OperationalDocument {
     // 获取项目根路径
     private static final String rootPath = System.getProperty("user.dir");
     // 构造文件夹路径
-    private static final String folderName = "HarvestDiary";  // 替换为你想要的文件夹名字
+    private static final String folderName = "HarvestDiary";
     private static final String folderPath = FileUtil.normalize(Paths.get(rootPath, folderName).toString());
 
-    public static void saveJSON(String content) {
-        String fileName = "user.json";  // 文件名
 
+
+    public static String encryptionString(String content){
         // 创建AES加密工具
-        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, Key.key);
-
+        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, Poetry.XinLuNan);
         // 加密
         byte[] encryptedData = aes.encrypt(content);
         content = Base64.encode(encryptedData);
 
+        return content;
+    }
+    public static void saveFile(String fileName, String content) {
+        //加密
+        content = encryptionString(content);
 
         // 创建文件夹（如果不存在）
         FileUtil.mkdir(folderPath);
@@ -46,13 +57,36 @@ public class OperationalDocument {
 
         System.out.println("Text file saved at: " + filePath);
 
+    }
+    public static void removeFile(String fileName){
+        // 创建文件夹（如果不存在）
+        FileUtil.mkdir(folderPath);
+        // 构造文件路径
+        Path filePath = Paths.get(folderPath, fileName);
+
+        FileUtil.del(filePath);
 
     }
+    public static void continuationFile(String content){
 
-    public static User readUser() {
+        content = readFile("app.config") + content;
+        System.out.println(content);
 
+        content = encryptionString(content);
+        // 创建文件夹（如果不存在）
+        FileUtil.mkdir(folderPath);
         // 构造文件路径
-        Path filePath = Paths.get(folderPath, "user.json");
+        Path filePath = Paths.get(folderPath, "app.config");
+
+        // 使用 Hutool 追加内容到文件
+        FileWriter writer = new FileWriter(FileUtil.file(filePath.toString()), "UTF-8");
+        System.out.println(content);
+
+        writer.write(content);
+    }
+    public static String readFile(String fileName){
+        // 构造文件路径
+        Path filePath = Paths.get(folderPath, fileName);
 
         // 使用 Hutool 的 FileReader 读取加密的 JSON 文件内容
         FileReader fileReader = new FileReader(filePath.toFile(), "UTF-8");
@@ -62,30 +96,21 @@ public class OperationalDocument {
         byte[] encryptedData = Base64.decode(encryptedContent);
 
         // 创建AES解密工具
-        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, Key.key);
+        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, Poetry.XinLuNan);
 
         // 解密
-        String decryptedJson = aes.decryptStr(encryptedData);
-
-        return JSONUtil.toBean(decryptedJson, User.class);
-    }
-
-    public static void saveFile(String fileName, String content){
-
-        // 创建文件夹（如果不存在）
-        FileUtil.mkdir(folderPath);
-
-        // 构造文件路径
-        Path filePath = Paths.get(folderPath, fileName);
-
-
-        // 使用 Hutool 的 FileWriter 写入文本内容
-        FileWriter fileWriter = new FileWriter(filePath.toFile(), "UTF-8");
-        fileWriter.write(content);
-
-        System.out.println("Text file saved at: " + filePath);
+        return aes.decryptStr(encryptedData);
     }
     public static boolean existFile(String fileName){
         return FileUtil.exist(FileUtil.file(folderPath, fileName));
+    }
+
+    public static void replace(String oldStr, String newStr, String fileName){
+        String s = OperationalDocument.readFile(fileName);
+        s = s.replace(oldStr, newStr);
+        OperationalDocument.saveFile(fileName, s);
+    }
+    public static ArrayList<String> readFileToList(String fileName){
+        return (ArrayList<String>) StrUtil.split(readFile(fileName), ';');
     }
 }
