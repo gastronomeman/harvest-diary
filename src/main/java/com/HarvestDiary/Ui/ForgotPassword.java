3599d7fcfaa1af5168f11b931ddedd79d225a587
gastronome <1305573134@qq.com>
2023-12-27 22:59:08 +0800

    添加“忘记密码”页面的jfx

--- src/main/java/com/HarvestDiary/Ui/ForgotPassword.java
+++ src/main/java/com/HarvestDiary/Ui/ForgotPassword.java
@@ -1,14 +1,35 @@
 package com.HarvestDiary.Ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.Getter;
+
+import java.util.Objects;
 
 public class ForgotPassword extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Getter//创建get方法
+    private static Stage forgotPasswordUiStage;
+
     @Override
     public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(
+                Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/ForgotPasswordUI.fxml"))
+        );
 
+        stage.setTitle("忘记密码");
+
+        Scene scene = new Scene(root);
+        scene.getRoot().requestFocus();
+        stage.setScene(scene);
+        stage.setResizable(false);//锁定窗口，禁止缩放
+        forgotPasswordUiStage = stage;
+        stage.show();
     }
 }
